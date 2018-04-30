9e401348f4c1785aa59d90b8c4643776bb80e1ba
Guest User <Guest@epicodus-10.local>
2018-04-30 16:21:56 -0700

    Add css

--- WorldData/Startup.cs
+++ WorldData/Startup.cs
@@ -30,6 +30,7 @@ namespace WorldData
         public void Configure(IApplicationBuilder app)
         {
             app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
